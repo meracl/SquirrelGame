82e466c9f24b42571be6500cbb9056c98d649a12
Marcel Franke <marcel.franke@hs-augsburg.de>
2017-04-04 18:46:35 +0200

    Replace GoodBeast.java

--- GoodBeast.java
+++ GoodBeast.java
@@ -1,14 +1,9 @@
 
 public class GoodBeast extends Entity {
-	final static int energy = 200;
-	final static int id = 0;
+	final static int ENERGY = 200;
 
-	public GoodBeast(int x, int y) {
-		super(id, energy, x, y);
+	public GoodBeast(int id, XY xy) {
+		super(id, ENERGY, xy);
 	}
 
-	public String toString() {
-		return "GoodBeast mit ID: " + getId() + " Energy: " + getEnergy() + " Koordinaten x: " + getX() + " y: "
-				+ getY();
-	}
 }
