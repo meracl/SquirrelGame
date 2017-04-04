1783c34bc9b12958210d6eb638c2a8e4a2952e5e
Marcel Franke <marcel.franke@hs-augsburg.de>
2017-04-04 18:45:18 +0200

    Replace Entity.java

--- Entity.java
+++ Entity.java
@@ -1,55 +1,52 @@
-import java.util.Scanner;
 
 abstract class Entity {
-	int id;
-	int energy;
-	int x;
-	int y;
+	private int id;
+	private int energy;
+	private XY xy;
 
-	public Entity(int id, int energy, int x, int y) {
+	public Entity(int id, int energy, XY xy) {
 		this.id = id;
 		this.energy = energy;
-		this.x = x;
-		this.y = y;
+		this.xy = xy;
 	}
 
-	static void nextStep(Entity entity) {
-		if (entity != null) {
-			int id = entity.id;
-			int dir = 0;
-			if (id == 5) {
-				Scanner step = new Scanner(System.in);
-				dir = step.nextInt();
-			} else if ((id == 0) | (id == 1) | (id == 6)) {
-				dir = (int) (Math.random() * 9);
-			}
-			switch (dir) {
-			case 1:
-				XY.moveDownLeft(entity);
-				break;
-			case 2:
-				XY.moveDown(entity);
-				break;
-			case 3:
-				XY.moveDownRight(entity);
-				break;
-			case 4:
-				XY.moveLeft(entity);
-				break;
-			case 6:
-				XY.moveRight(entity);
-				break;
-			case 7:
-				XY.moveUpLeft(entity);
-				break;
-			case 8:
-				XY.moveUp(entity);
-				break;
-			case 9:
-				XY.moveUpRight(entity);
-			}
-		}
-	}
+	// static void nextStep(Entity entity) {
+	// if (entity != null) {
+	// int id = entity.id;
+	// int dir = 5;
+	// if (id == 5) {
+	// Scanner step = new Scanner(System.in);
+	// dir = step.nextInt();
+	// } else if ((id == 0) | (id == 1) | (id == 6)) {
+	// dir = (int) (Math.random() * 9);
+	// }
+	// switch (dir) {
+	// case 1:
+	// XY.moveDownLeft(entity);
+	// break;
+	// case 2:
+	// XY.moveDown(entity);
+	// break;
+	// case 3:
+	// XY.moveDownRight(entity);
+	// break;
+	// case 4:
+	// XY.moveLeft(entity);
+	// break;
+	// case 6:
+	// XY.moveRight(entity);
+	// break;
+	// case 7:
+	// XY.moveUpLeft(entity);
+	// break;
+	// case 8:
+	// XY.moveUp(entity);
+	// break;
+	// case 9:
+	// XY.moveUpRight(entity);
+	// }
+	// }
+	// }
 
 	int getId() {
 		return id;
@@ -59,16 +56,20 @@ abstract class Entity {
 		return energy;
 	}
 
-	int getX() {
-		return x;
+	XY getXy() {
+		return xy;
 	}
 
-	int getY() {
-		return y;
+	void updateEnergy(int delta) {
+		energy = energy + delta;
 	}
 
-	void setEnergy(int x) {
-		energy = x;
+	void nextStep() {
+
+	}
+
+	public String toString() {
+		return this.getClass() + " mit ID: " + getId() + " Energy: " + getEnergy() + " Koordinaten " + xy;
 	}
 
 }
