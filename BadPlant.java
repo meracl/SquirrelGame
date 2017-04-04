52b82a61f38ff455aca621d9404bca95d3d28a95
Marcel Franke <marcel.franke@hs-augsburg.de>
2017-04-04 18:44:49 +0200

    Replace BadPlant.java

--- BadPlant.java
+++ BadPlant.java
@@ -1,14 +1,9 @@
 
 public class BadPlant extends Entity {
-	final static int energy = -100;
-	final static int id = 3;
+	final static int ENERGY = -100;
 
-	public BadPlant(int x, int y) {
-		super(id, energy, x, y);
+	public BadPlant(int id, XY xy) {
+		super(id, ENERGY, xy);
 	}
 
-	public String toString() {
-		return "BadPlant mit ID: " + getId() + " Energy: " + getEnergy() + " Koordinaten x: " + getX() + " y: "
-				+ getY();
-	}
 }
