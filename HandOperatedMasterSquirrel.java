3db8a6af29565fccef271d41caaca96f5ce9fd14
Marcel Franke <marcel.franke@hs-augsburg.de>
2017-04-06 16:40:07 +0200

    Replace HandOperatedMasterSquirrel.java

--- HandOperatedMasterSquirrel.java
+++ HandOperatedMasterSquirrel.java
@@ -1,25 +1,37 @@
 import java.util.Scanner;
 
-public class HandOperatedMasterSquirrel {
-	public static XY squirrelStep(XY xy) {
+public class HandOperatedMasterSquirrel extends MasterSquirrel {
+	private static XY xy = new XY(0, 0);
+
+	public HandOperatedMasterSquirrel(int id) {
+		super(id, xy);
+	}
+
+	void nextStep() {
+		XY newxy=new XY(xy);
 		Scanner step = new Scanner(System.in);
 		char dir = step.next(".").charAt(0);
 		switch (dir) {
 		case 'w':
-			xy.y--;
+			newxy.y--;
 			break;
 		case 'a':
-			xy.x--;
+			newxy.x--;
 			break;
 		case 's':
-			xy.y++;
+			newxy.y++;
 			break;
 		case 'd':
-			xy.x++;
+			newxy.x++;
 			break;
 		default:
 			System.out.println("keine gültige Richtung");
 		}
-		return xy;
+		boolean move = EntitySet.possibleMove(newxy.x,newxy.y,getId());
+		if (move){
+			this.xy.x=newxy.x;
+			this.xy.y=newxy.y;
+		}
+		
 	}
 }
