0e44d22520cf98204ac6a2e31ddf57f056441a8f
Marcel Franke <marcel.franke@hs-augsburg.de>
2017-04-06 16:41:08 +0200

    Replace Main.java

--- Main.java
+++ Main.java
@@ -1,10 +1,13 @@
 public class Main {
 
 	public static void main(String[] args) {
-		MasterSquirrel masterSquirrel = new MasterSquirrel(1);
-		System.out.println(masterSquirrel);
-		masterSquirrel.nextStep();
-		System.out.println(masterSquirrel);
+		EntitySet.createEntitys();
+		while (true) {
+			run();
+		}
+	}
 
+	public static void run() {
+		EntitySet.runAll();
 	}
 }
