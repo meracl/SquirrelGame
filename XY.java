62a78779d97c7d3830a10cbbda0915e89cba329c
Marcel Franke <marcel.franke@hs-augsburg.de>
2017-04-06 16:42:45 +0200

    Replace XY.java

--- XY.java
+++ XY.java
@@ -6,51 +6,14 @@ final public class XY {
 		this.x = x;
 		this.y = y;
 	}
+	public XY(XY xy) {
+		this.x = xy.x;
+		this.y = xy.y;
+	}
 
 	public String toString() {
 		return "x: " + this.x + " y: " + this.y;
 	}
-	// static Entity moveRight(Entity entity) {
-	// entity.x++;
-	// return entity;
-	// }
-	//
-	// static Entity moveLeft(Entity entity) {
-	// entity.x--;
-	// return entity;
-	// }
-	//
-	// static Entity moveDown(Entity entity) {
-	// entity.y++;
-	// return entity;
-	// }
-	//
-	// static Entity moveUp(Entity entity) {
-	// entity.y--;
-	// return entity;
-	// }
-	//
-	// static Entity moveUpRight(Entity entity) {
-	// entity.x++;
-	// entity.y--;
-	// return entity;
-	// }
-	//
-	// static Entity moveUpLeft(Entity entity) {
-	// entity.x--;
-	// entity.y--;
-	// return entity;
-	// }
-	//
-	// static Entity moveDownRight(Entity entity) {
-	// entity.x++;
-	// entity.y++;
-	// return entity;
-	// }
-	//
-	// static Entity moveDownLeft(Entity entity) {
-	// entity.x--;
-	// entity.y++;
-	// return entity;
-	// }
+
+
 }
