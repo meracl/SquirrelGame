4988ebe3bbafd3127c25048e2282442e3e84cca9
Marcel Franke <marcel.franke@hs-augsburg.de>
2017-04-12 18:04:41 +0200

    Replace MinniSquirrel.java

--- MinniSquirrel.java
+++ MinniSquirrel.java
@@ -1,7 +1,19 @@
 
 public class MinniSquirrel extends Squirrel {
-	public MinniSquirrel(int id, int energy, XY xy) {
+	private int parentId;
+
+	public MinniSquirrel(int id, int energy, XY xy, int pid) {
 		super(id, energy, xy);
+		parentId = pid;
+	}
+
+	public String toString() {
+		return this.getClass() + " mit ID: " + getId() + " Energy: " + getEnergy() + " Koordinaten " + getXy()
+				+ " ParentID: " + parentId;
+	}
+
+	public int getPid() {
+		return parentId;
 	}
 
 }
