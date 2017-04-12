ac19b1c12f5a9de9a0dadd245707c426ef1cee73
Marcel Franke <marcel.franke@hs-augsburg.de>
2017-04-12 18:05:23 +0200

    Replace XY.java

--- XY.java
+++ XY.java
@@ -1,11 +1,12 @@
 final public class XY {
-	public int x;
-	public int y;
+	public final int x;
+	public final int y;
 
 	public XY(int x, int y) {
 		this.x = x;
 		this.y = y;
 	}
+
 	public XY(XY xy) {
 		this.x = xy.x;
 		this.y = xy.y;
@@ -15,5 +16,29 @@ final public class XY {
 		return "x: " + this.x + " y: " + this.y;
 	}
 
+	public static XY addXy(XY xy1, XY xy2) {
+		int newx = xy1.x + xy2.x;
+		int newy = xy1.y + xy2.y;
+		return new XY(newx, newy);
+	}
+
+	public static XY move(XY xy) {
+		XY vec = randomVec();
+		return addXy(xy, vec);
+	}
+
+	public static XY randomVec() {
+		int i = (int) (Math.random() * 3) - 1;
+		int j = (int) (Math.random() * 3) - 1;
+
+		return new XY(i, j);
+	}
+
+	public boolean equals(XY xy) {
+		if (this.x == xy.x && this.y == xy.y) {
+			return true;
+		}
+		return false;
+	}
 
 }
