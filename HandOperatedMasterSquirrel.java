40d29540dedd73c6d0de6aa1dee41dd9393fbc61
Marcel Franke <marcel.franke@hs-augsburg.de>
2017-04-12 18:03:41 +0200

    Replace HandOperatedMasterSquirrel.java

--- HandOperatedMasterSquirrel.java
+++ HandOperatedMasterSquirrel.java
@@ -1,37 +1,56 @@
 import java.util.Scanner;
 
 public class HandOperatedMasterSquirrel extends MasterSquirrel {
-	private static XY xy = new XY(0, 0);
+	public static XY xy = new XY(0, 3);
 
 	public HandOperatedMasterSquirrel(int id) {
 		super(id, xy);
 	}
 
-	void nextStep() {
-		XY newxy=new XY(xy);
+	public void nextStep() {
+		System.out.println("'w','a','s','d'oder'n' eingeben.");
 		Scanner step = new Scanner(System.in);
-		char dir = step.next(".").charAt(0);
+		char d = step.next(".").charAt(0);
+		if (d == 'w' | d == 'a' | d == 's' | d == 'd') {
+			doMove(d);
+		} else if (d == 'n') {
+			System.out.println("Bitte Wert zwischen 100 und " + (this.getEnergy() - 1) + " eingeben");
+			Scanner enScan = new Scanner(System.in);
+			int givenEnergy = Integer.parseInt(enScan.nextLine());
+			while (givenEnergy >= this.getEnergy() | givenEnergy < 100) {
+				System.out.println("Zahl nicht zulässig nochmal versuchen");
+				Scanner enScan2 = new Scanner(System.in);
+				givenEnergy = Integer.parseInt(enScan2.nextLine());
+			}
+			EntitySet.addEntity(this.createMinni(givenEnergy));
+		} else {
+			System.out.println("keine gültige Eingabe");
+		}
+
+	}
+
+	public void doMove(char dir) {
+		XY vector = new XY(0, 0);
 		switch (dir) {
 		case 'w':
-			newxy.y--;
+			vector = new XY(0, -1);
 			break;
 		case 'a':
-			newxy.x--;
+			vector = new XY(-1, 0);
 			break;
 		case 's':
-			newxy.y++;
+			vector = new XY(0, 1);
 			break;
 		case 'd':
-			newxy.x++;
+			vector = new XY(1, 0);
 			break;
 		default:
 			System.out.println("keine gültige Richtung");
 		}
-		boolean move = EntitySet.possibleMove(newxy.x,newxy.y,getId());
-		if (move){
-			this.xy.x=newxy.x;
-			this.xy.y=newxy.y;
+		XY newxy = XY.addXy(getXy(), vector);
+		boolean move = Main.game.possibleMoveSq(newxy, getId());
+		if (move) {
+			setXy(newxy);
 		}
-		
 	}
 }
