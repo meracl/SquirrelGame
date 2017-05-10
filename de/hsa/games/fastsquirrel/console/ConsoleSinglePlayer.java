27efbc330caa53ff755f72517639136fd54bae14
Marcel <Marcel.Franke@hs-augsburg.de>
2017-05-10 10:48:02 +0200

    reflection fertig bis auf spawn_minni

--- de/hsa/games/fastsquirrel/console/ConsoleSinglePlayer.java
+++ de/hsa/games/fastsquirrel/console/ConsoleSinglePlayer.java
@@ -6,6 +6,8 @@ import de.hsa.games.fastsquirrel.core.Board;
 import de.hsa.games.fastsquirrel.core.Game;
 import de.hsa.games.fastsquirrel.core.HandOperatedMasterSquirrel;
 
+import java.lang.reflect.Method;
+
 public class ConsoleSinglePlayer extends Game {
     private HandOperatedMasterSquirrel player = new HandOperatedMasterSquirrel(1);
     private XY squirrelMove = new XY(0, 0);
@@ -26,59 +28,30 @@ public class ConsoleSinglePlayer extends Game {
 
     protected void processInput() {
         Command command;
-        Object[] params;
-        GameCommandType commandType;
+        Object[] params = new Object[0];
         try {
             command = ui.getCommand();
+
 
         } catch (ScanExceptions e) {
             command = null;
         }
-        if (command != null) {
+        if(command!=null){
             params = command.getParams();
-            commandType = (GameCommandType) command.getCommandType();
-            switch (commandType) {
-                case UP:
-                    this.squirrelMove = new XY(-1, 0);
-                    player.nextStep(getState().getContext(), squirrelMove);
-                    updateAfterMaster();
-                    break;
-                case RIGHT:
-                    this.squirrelMove = new XY(0, 1);
-                    player.nextStep(getState().getContext(), squirrelMove);
-                    updateAfterMaster();
-                    break;
-                case DOWN:
-                    this.squirrelMove = new XY(1, 0);
-                    player.nextStep(getState().getContext(), squirrelMove);
-                    updateAfterMaster();
-                    break;
-                case LEFT:
-                    this.squirrelMove = new XY(0, -1);
-                    player.nextStep(getState().getContext(), squirrelMove);
-                    updateAfterMaster();
-                    break;
-                case HELP:
-                    help();
-                    break;
-                case ALL:
-                    all();
-                    break;
-                case EXIT:
-                    System.exit(0);
-                case MASTER_ENERGY:
-                    masterEnergy();
-                    break;
-                case SPAWN_MINI:
-                    spawnMini(params);
-                    break;
+        }
+        if (command != null) {
+            try {
+                GameCommandType commandType = (GameCommandType) command.getCommandType();
+                Method method = this.getClass().getDeclaredMethod(commandType.getName(), (Class<?>[]) params);
+                method.invoke(this);
+            } catch (Exception e) {
+                System.out.println("test");
             }
         }
-
-
     }
 
-    private void spawnMini(Object[] params) {
+    @SuppressWarnings("unused")
+    private void n(Object[] params) {
         int energy = (int) params[0];
         player.updateEnergy(-energy);
         XY xy;
@@ -89,14 +62,49 @@ public class ConsoleSinglePlayer extends Game {
         board.createMinni(player.getId(), energy, xy);
     }
 
+    @SuppressWarnings("unused")
+    private void w() {      //up
+        this.squirrelMove = new XY(-1, 0);
+        player.nextStep(getState().getContext(), squirrelMove);
+        updateAfterMaster();
+
+    }
+
+    @SuppressWarnings("unused")
+    private void a() {      //left
+        this.squirrelMove = new XY(0, -1);
+        player.nextStep(getState().getContext(), squirrelMove);
+        updateAfterMaster();
+
+    }
+
+    @SuppressWarnings("unused")
+    private void s() {      //down
+        this.squirrelMove = new XY(1, 0);
+        player.nextStep(getState().getContext(), squirrelMove);
+        updateAfterMaster();
+
+    }
+
+    @SuppressWarnings("unused")
+    private void d() {      //right
+        this.squirrelMove = new XY(0, 1);
+        player.nextStep(getState().getContext(), squirrelMove);
+        updateAfterMaster();
+
+    }
+
+    @SuppressWarnings("unused")
     private void masterEnergy() {
         System.out.println("Your Energy: " + player.getEnergy());
     }
 
+    @SuppressWarnings("unused")
     private void all() {
         System.out.println(getState().getBoard().toString());
     }
 
+    @SuppressWarnings("unused")
     private static void help() {
         CommandTypeInfo[] commandTypeInfos = GameCommandType.values();
         System.out.println("These are the usable commands:");
