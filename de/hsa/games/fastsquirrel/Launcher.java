53d940a455b41c75306b8b3179b037b75c3d1e48
Marcel <Marcel.Franke@hs-augsburg.de>
2017-05-11 13:53:54 +0200

    reflection abgeschlossen

--- de/hsa/games/fastsquirrel/Launcher.java
+++ de/hsa/games/fastsquirrel/Launcher.java
@@ -2,7 +2,7 @@ package de.hsa.games.fastsquirrel;
 
 import de.hsa.games.fastsquirrel.console.ConsoleSinglePlayer;
 
-public class Main {
+public class Launcher {
     public static void main(String[] args) {
 
         ConsoleSinglePlayer game = new ConsoleSinglePlayer();
